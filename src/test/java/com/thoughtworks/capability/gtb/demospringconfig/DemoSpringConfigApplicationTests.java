7c81182a129f663efd96c447690d1644fdc50e53
Shen Liao <shen.liao@thoughtworks.com>
2020-09-20 22:09:20 +0800

    [Nelson Liao] feat: finish exercise-2 method-1

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,27 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoSpringConfigApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void shouldBeBasicWhenLevelNumLessThanOne() throws Exception {
+		String result = mockMvc.perform(get("/level"))
+				.andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		Assertions.assertEquals("basic", result);
 	}
 
 }
